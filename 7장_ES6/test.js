c662c7e911041bba92f7540f13b5c3768a6a0ed9
LEE SANG-CHUL <1ilsang@naver.com>
2018-04-17 16:57:12 +0900

    es6

--- 7장_ES6/test.js
+++ 7장_ES6/test.js
@@ -1,9 +1,15 @@
-var msg = "GLOBAL";
-function outer(a) {
-    var msg = "OUTER";
-    console.log(msg);
-    if (true) {
-        var msg = "BLOCK";
-        console.log(msg);
+var age = 1;
+function Person(name, yearCount) {
+    this.name = name;
+    this.age = 0;
+    var incrAge = function(){
+        this.age++;
+    }
+    for (var i=1; i <= yearCount; i++) {
+        incrAge();
     }
 }
+
+p1 = new Person("홍길동",1);
+//--this.age는 0이 출력됨.
+console.log(p1.name + "님의 나이 : " + p1.age);
